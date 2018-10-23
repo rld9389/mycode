43b6e2f4e5f957682cfcfb6cbee9bb267bd2fb9e
Rebecca <rld9389@gmail.com>
2018-10-23 14:27:36 -0400

    iftest 2

---
+++ if-test/if-hostname.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#hostname = 'MTG'
+
+#if hostname == 'MTG':
+#	print('The hostname was found to be mtg')
+
+
+#accept input from user
+hostnameinput = input('What is your hostname? ')
+
+if hostnameinput == "mtg" or "MTG" or "mTG" or "MTg" or "mTg" or "MtG":
+#if hostname is anything then go to next
+#	hostnameinput == True
+	print("hostname matches expected config")
+#prints if true
+#elif hostnameinput != "mtg" or "MTG" or "mTG" or "MTg" or "mTg" or "MtG":
+#	print(hostnameinput)
+#	exit()
+
+print("Exiting the script.")
+
+
+
---
+++ mix-list3/animal-list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+animal_list = ["Fox","Fly","Ant","Bee","Cat","Dog","Yak","Cow","Hen","Koi","Hog","Jay","Kit"]
+
+list1 = ['cisco_nxos', 'arista_eos', 'cisco_ios']
+
+
+print(animal_list[0] + " " + animal_list[1] + " " + animal_list[2] + " " + animal_list[3] + " " + animal_list[4] + " " + animal_list[5] + " " + animal_list[6] + " " + animal_list[7] + " " + animal_list[8] + " " + animal_list[9] + " " + animal_list[10] + " " + animal_list[11] + " " + animal_list[12])
+
